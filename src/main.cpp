25303fb12f10c827d101cb0d22c3123c1f8d3cf6
CristianBold4 <boldrincristian00@gmail.com>
2024-03-11 09:41:54 +0000

    fixed minors on turing

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "Utils.h"
 #include <fstream>
 #include <string>
-
+#include <chrono>
 
 void run_tonic_algo(std::string &dataset_path, TriangleSampler &algo) {
 
@@ -62,6 +62,7 @@ int main(int argc, char **argv) {
     double time;
     bool edge_oracle_flag = false;
     TriangleSampler tonic_algo (random_seed, memory_budget, alpha, beta);
+    int size_oracle;
 
     if (oracle_type == "nodes") {
         emhash5::HashMap<int, int> node_oracle;
@@ -70,6 +71,7 @@ int main(int argc, char **argv) {
                 std::chrono::high_resolution_clock::now() - start)).count()) / 1000;
         printf("Node Oracle successfully read in time %.3f! Size of the oracle = %d nodes\n",
                time, node_oracle.size());
+	size_oracle = node_oracle.size();
         tonic_algo.set_node_oracle(node_oracle);
     } else if (oracle_type == "edges") {
         edge_oracle_flag = true;
@@ -79,6 +81,7 @@ int main(int argc, char **argv) {
                 std::chrono::high_resolution_clock::now() - start)).count()) / 1000;
         printf("Edge Oracle successfully read in time %.3f! Size of the oracle = %d edges\n",
                 time, edge_oracle.size());
+	size_oracle = edge_oracle.size();
         tonic_algo.set_edge_oracle(edge_oracle);
     } else {
         std::cerr << "Error! Oracle type must be nodes or edges\n";
@@ -95,12 +98,12 @@ int main(int argc, char **argv) {
     // -- write results
     std::ofstream outFile(output_path + "_global_count.txt", std::ios::app);
     if (edge_oracle_flag)
-        outFile << "Tonic Algo with Edge Oracle, memory budget = " << memory_budget << "\n";
+        outFile << "Tonic Algo with Edge Oracle: " << oracle_path << " of size: " << size_oracle << ", Memory Budget = " << memory_budget << "\n";
     else
-        outFile << "Tonic Algo with Node Oracle, memory budget = " << memory_budget << "\n";
+        outFile << "Tonic Algo with Node Oracle: " << oracle_path << " of size: " << size_oracle << ", Memory Budget = " << memory_budget << "\n";
 
     outFile << std::fixed << "Estimated Triangles = " << tonic_algo.get_global_triangles() << "\n" <<
-            "Time = " << time << "\n";
+            "Time = " << time << " s\n";
     outFile.close();
 
     return 0;
